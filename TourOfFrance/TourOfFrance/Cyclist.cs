f15971106f3a283337a547b265a70287f0d3c6ef
maximilienviallon <42693169+maximilienviallon@users.noreply.github.com>
2019-09-27 14:50:19 +0200

    added cyclist class

---
+++ TourOfFrance/TourOfFrance/Cyclist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourDeFranceCyclist
+{
+
+
+    class Cyclist
+    {
+        private string name;
+        private string gender;
+        private string countryOfOg;
+        private string resultTime;
+        private string endPosition;
+        public Cyclist(string name, string gender, string country, string result, string endPos)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.countryOfOg = country;
+            this.resultTime = result;
+            this.endPosition = endPos;
+        }
+
+    }
+
+
+}
+
